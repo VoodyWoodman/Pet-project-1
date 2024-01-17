9840bdfce00730986ceced89d1d2195ba046826e
VoodyWoodman <155814572+VoodyWoodman@users.noreply.github.com>
2024-01-17 18:25:33 +0300

    Комментарий

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Проект №1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header>
+        <h1>Проект №1</h1>
+    </header>
+
+    <nav>
+        <a href="#section1">Section 1</a>
+        <a href="#section2">Section 2</a>
+        <a href="autorization.html">Вход</a>
+        <a href="registration.html">Регистрация</a>
+    </nav>
+
+    <section id="section1">
+        <h2>Раздел 1</h2>
+        <p>Контент для раздела 1.</p>
+    </section>
+
+    <section id="section2">
+        <h2>Раздел 2</h2>
+        <p>Контент для раздела 2.</p>
+    </section>
+
+
+    <footer>
+        <p>&copy; 2024 Your Website. Все права защищены.</p>
+    </footer>
+
+</body>
+</html>
